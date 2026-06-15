826a45e1d322a0cf4dd28fb7ea5f242068d4b59c
Andi Sugara <48427117+andisugara@users.noreply.github.com>
2026-06-16 02:23:07 +0700

    Update uploadImages.js

--- src/middleware/uploadImages.js
+++ src/middleware/uploadImages.js
@@ -1,10 +1,8 @@
 const Multer = require('multer');
-
 const multer = Multer({
-    storage: Multer.MemoryStorage,
+    storage: Multer.memoryStorage(),
     limits: {
         fileSize: 5 * 1024 * 1024
     }
 })
-
 module.exports = multer;
