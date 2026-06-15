31074552aa1dbe49fc52e9712d434a4e6245e31d
Andi Sugara <48427117+andisugara@users.noreply.github.com>
2026-06-16 01:42:06 +0700

    Update uploadToBucket.js

--- src/utils/uploadToBucket.js
+++ src/utils/uploadToBucket.js
@@ -1,44 +1,64 @@
 const { Storage } = require('@google-cloud/storage');
 
-const storage = new Storage();
+const storage = new Storage({
+    projectId: 'z-healths',
+    keyFilename: process.env.GCP_CREDENTIALS
+});
 
-const bucketName = "z-health-496809.firebasestorage.app";
+const bucketName = "z-health-bucket23";
 const bucket = storage.bucket(bucketName);
 
 function imgUrlBucket(filename) {
-    return `https://firebasestorage.googleapis.com/v0/b/${bucketName}/o/${encodeURIComponent(filename)}?alt=media`;
+    return `https://storage.googleapis.com/${bucketName}/${filename}`;
 }
 
 const bucketUpload = {};
-const nodePath = require('path'); 
+
 bucketUpload.uploadToBucket = (req, res, next) => {
+
     if (!req.file) return next();
-    const user_id = req.params.id;
-    const imgName = `avatar-${user_id}${nodePath.extname(req.file.originalname)}`; // ✅ pakai nodePath
-    let folder; // ✅ ganti nama variable dari 'path' ke 'folder'
+
+    const timeStamp = new Date().getTime()
+    const ext = path.extname(req.file.originalname);
+    const imgName = `avatar-${req.params.id}${ext}`;
+
+    let path;
     if (req.file.fieldname == 'profile_img') {
-        folder = 'profile_img/'
-    }
+        path = 'profile_img/'
+    } 
+    // else if (req.file.fieldname == 'image') {
+    //     path = 'donation_img/'
+
+    // } else if (req.file.fieldname == 'sell_img') {
+    //     path = 'sell_img/'
+
+    // }
         
-    const gcsname = folder + imgName;
+    const gcsname = path + imgName;
     const file = bucket.file(gcsname);
+
 
     const stream = file.createWriteStream({
         metadata: {
-            contentType: req.file.mimetype || 'image/jpeg', // ✅ fallback ke image/jpeg
+            contentType: req.file.mimetype,
         },
     });
-    
+
     stream.on("error", (err) => {
         req.file.cloudStorageError = err;
         next(err);
     });
+
     stream.on("finish", () => {
+        req.file.cloudStorageObject = gcsname;
         req.file.cloudStoragePublicUrl = imgUrlBucket(gcsname);
-        next();
+        file.makePublic().then(() => {
+            req.file.cloudStoragePublicUrl = imgUrlBucket(gcsname);
+            next();
+        });
     });
+
     stream.end(req.file.buffer);
 };
-
 
 module.exports = bucketUpload;
